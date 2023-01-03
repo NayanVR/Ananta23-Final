ec27534a945d5cba47e15b14633620798e941066
NayanVR <nvradadiya281@gmail.com>
2023-01-04 01:35:54 +0530

    Events page working

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Swift from '../assets/photos/upshots/swift.png'
 
-function EventCard() {
+function EventCard({ index }) {
   return (
-    <div className='w-3/4 h-[70vh] flex bg-primary-light-3 border-primary border-2 rounded-md'>
+    <div style={{ zIndex: index }} className='w-3/4 h-[70vh] sticky top-0 left-0 p-12 flex gap-4 items-center bg-primary-light-3 border-primary-light-1 border-2 rounded-md'>
       <div className='flex-[2]'>
         <h1 className="font-heading text-4xl font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent">
           STEM Showcase
         </h1>
-        <p>
+        <p className='my-6'>
           Science, technology, engineering and mathematics are the four basic topics we see around ourselves everyday. Bring out working models, business models and prototypes to display your ideas and potential talent.
         </p>
         <button type='submit' className='py-2 px-4 bg-primary-dark-1 text-white rounded-md'>
@@ -18,7 +19,7 @@ function EventCard() {
         </button>
       </div>
       <div className='flex-[1]'>
-
+        <img src={Swift} alt="Swift Fingers" className='w-full h-full object-cover' />
       </div>
     </div>
   )
--- client/src/pages/Inertia.jsx
+++ client/src/pages/Inertia.jsx
@@ -3,7 +3,10 @@ import EventCard from '../components/EventCard'
 
 function Inertia() {
   return (
-    <EventCard />
+    <div className='relative h-screen overflow-y-scroll'>
+      <EventCard index={1} />
+      <EventCard index={2} />
+    </div>
   )
 }
 
