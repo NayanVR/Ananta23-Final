e0d77277db84db423e19142592bf646191651e01
Pratham1777 <prthmpthk777@gmail.com>
2023-02-02 01:45:04 +0530

    team page done

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Anantalogo from '../assets/logos/ananta_logo.svg'
-import Renish from '../assets/photos/team/Renish.jpg'
+// import Renish from '../assets/photos/team/Renish.jpg'
+// import Khyati from '../assets/photos/team/Khyati.jpg'
 import User from '../assets/icons/User.svg'
 import EmailIcon from '../assets/icons/Email.svg'
 import Phone from '../assets/icons/Phone.svg'
@@ -12,6 +13,10 @@ function Team() {
     return (
         <>
             {/* leader */}
+            <h1 className="font-heading text-7xl text-center my-12 text-[2rem] font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent">
+                Contact Us
+            </h1>
+
             <div className='flex flex-col items-center justify-center  '>
                 <h1 className="font-heading text-center my-12 text-[2rem] font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent">
                     LEADERSHIP TEAM
@@ -161,7 +166,7 @@ function TeamCard({ info }) {
     return (
         <div className='group relative items-center border-2 border-primary justify-center overflow-hidden cursor=pointer hover:shadow-xl hover:shadow-black/30  rounded-md'>
             <div className='h-96 w-72'>
-                <img src={Renish} className='h-full w-full object-cover group-hover:rotate-1 group-hover:scale-125    transition-transform ' alt="" />
+                <img src={`/team/${info.image}`} className='h-full w-full object-cover group-hover:rotate-1 group-hover:scale-125    transition-transform ' alt="" />
             </div>
             <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70'></div>
             <div className='absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[63%] group-hover:translate-y-0 transition-all'>
@@ -171,7 +176,7 @@ function TeamCard({ info }) {
                 <div className=' text-white  opacity-0 group-hover:opacity-100 transition-opacity '>
                     <div className='flex pb-1 justify-center gap-3'>
 
-                        <p className='text-sm'>{info.email}</p>
+                        <a href={`mailto:${info.email}`} className='text-sm'>{info.email}</a>
                     </div>
                     <div className='flex pb-2 justify-center gap-3'>
 
@@ -180,17 +185,13 @@ function TeamCard({ info }) {
 
                     <div className='flex pb-2 gap-3 justify-center mt-2'>
 
-                        <a href='anantagsfcu.in'>
-                            <div className='p-2 bg-white rounded-md'>
-                                <img src={EmailIcon} className='h-4' alt="" />
-                            </div>
-                        </a>
-                        <a href=''>
+
+                        <a href={`https://wa.me/+91${info.phone}`}>
                             <div className='p-2 bg-white rounded-md'>
                                 <img src={WP} className='h-4' alt="" />
                             </div>
                         </a>
-                        <a href=''>
+                        <a href={`${info.linkedin}`}>
                             <div className='p-2 bg-white rounded-md'>
                                 <img src={Linkedin} className='h-4' alt="" />
                             </div></a>
