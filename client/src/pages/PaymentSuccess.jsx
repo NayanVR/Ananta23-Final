bd9eef6269a0043c016f382ecb51b861844eef75
Pratham1777 <prthmpthk777@gmail.com>
2023-02-11 01:13:13 +0530

    Payment Success

--- client/src/pages/PaymentSuccess.jsx
+++ client/src/pages/PaymentSuccess.jsx
@@ -24,7 +24,7 @@ function PaymentSuccess() {
 		const getdetails = async () => {
 			console.log("varada");
 			const data = await fetch(
-				server_URL+"/api/payment/checkPaymentStatus",
+				server_URL + "/api/payment/checkPaymentStatus",
 				{
 					method: "POST",
 					headers: {
@@ -62,46 +62,46 @@ function PaymentSuccess() {
 					<h1 className="text-2xl font-extrabold text-green-600 flex flex-col justify-between items-center">
 						Payment Successful
 					</h1>
-					<div className="max-w-[1200px] m-auto grid grid-cols-1 md:grid-cols-2 md:gap-y-8 lg:grid-cols-3 place-items-center my-4">
-						<table className="items-center">
+					<div className="">
+						<div className="">
 							{console.log(PayDetails)}
 							{PayDetails.length != 0 ? (
 								PayDetails.map((data) => {
 									return (
-									<tbody className="text-xs">
-										<tr>
-											<td className="w-full">OrderID</td>
-											<td>{data.OrderID}</td>
-										</tr>
-										<tr>
-											<td className="w-full">PassType</td>
-											<td>{data.PassType}</td>
-										</tr>
-										{/* <tr>
-											<td className="w-full">Transaction Status</td>
-											<td>{data.TxnStatus}</td>
+										<div className="flex flex-col justify-center items-center text-sm sm:text-lg ">
+											<div className="flex flex-wrap">
+												<p className="font-bold">PassType :</p>
+												<p>{data.PassType}</p>
+											</div>
+											<div className="flex flex-col justify-center items-center">
+												<p className="font-bold">OrderID :</p>
+												<p>{data.OrderID}</p>
+											</div>
+											{/* <tr>
+											<p className="w-full">Transaction Status</p>
+											<p>{data.TxnStatus}</p>
 										</tr> */}
-										<tr>
-											<td className="w-full">Transaction ID</td>
-											<td>{data.TxnID}</td>
-										</tr>
-										{/* <tr>
-											<td className="w-full">Date</td>
-											<td>{data.TxnDate}</td>
+											<div className="flex flex-wrap justify-center items-center">
+												<p className="font-bold">Transaction ID :</p>
+												<p>{data.TxnID}</p>
+											</div>
+											{/* <tr>
+											<p className="w-full">Date</p>
+											<p>{data.TxnDate}</p>
 										</tr> */}
-										
-									</tbody>);
+
+										</div>);
 								})
 							) : (
 								<tr className="m-auto">
-									<td>Record not found</td>
+									<p className="flex items-center justify-center">Record not found</p>
 								</tr>
 							)}
-						</table>
+						</div>
 					</div>
 				</div>
 
-				<button className='relative before:content-[""] before:absolute before:w-full before:h-full before:top-0 before:bg-gradient-to-r before:from-transparent before:to-transparent before:via-primary-light-1 before:-left-full before:hover:left-full before:transition-all before:duration-500 hover:shadow-lg hover:shadow-primary-light-2 transition-all overflow-hidden bg-gradient-to-b from-primary-dark-1 to-primary-dark-2 py-3 mt-4 w-full bg-primary-dark-1 font-heading font-bold text-white rounded-lg'>
+				<button className='relative before:content-[""] before:absolute before:w-full before:h-full before:top-0 before:bg-gradient-to-r before:from-transparent before:to-transparent before:via-primary-light-1 before:-left-full before:hover:left-full before:transition-all before:duration-500 hover:shadow-lg hover:shadow-primary-light-2 transition-all overflow-hidden bg-gradient-to-b from-primary-dark-1 to-primary-dark-2 py-3 mt-4 w-1/3 bg-primary-dark-1 font-heading font-bold text-white rounded-lg'>
 					Download Pass
 				</button>
 			</div>
