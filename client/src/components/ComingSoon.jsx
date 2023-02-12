da294d9dc47598453248f0fa40dd225ac589f5b7
Pratham1777 <prthmpthk777@gmail.com>
2023-02-13 04:53:39 +0530

    coming soon

--- client/src/components/ComingSoon.jsx
+++ client/src/components/ComingSoon.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Coming from '../assets/icons/comingsoon.svg'
+import Coming from '../assets/icons/coming1.png'
 
 function ComingSoon() {
     return (
         <>
             <div className='flex h-[calc(100vh-3.5rem)] justify-center items-end'>
-                <img src={Coming} className="overflow-hidden h-[100vh]  sm:items-center " alt="" />
+                <img src={Coming} className="flex overflow-hidden h-[100vh] justify-center items-center sm:items-center " alt="" />
                 {/* <h1 className='flex justify-center items-center text-4xl font-heading font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent'>Coming Soon</h1> */}
             </div>
         </>
