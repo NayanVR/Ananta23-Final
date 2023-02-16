57fdf2f2b6726664e217708715afa70d01ab53e3
Pratham1777 <prthmpthk777@gmail.com>
2023-02-16 23:08:03 +0530

    few changes

--- client/src/pages/Equilibrium.jsx
+++ client/src/pages/Equilibrium.jsx
@@ -4,6 +4,9 @@ import ComingSoon from '../components/ComingSoon'
 function Equilibrium() {
     return (
         <>
+            <h1 className="font-heading text-center my-12 text-[2rem] font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent">
+                Equilibrium: Guests Lectures
+            </h1>
             <ComingSoon />
         </>
     )
--- client/src/pages/Inertia.jsx
+++ client/src/pages/Inertia.jsx
@@ -186,7 +186,7 @@ function Inertia() {
 	return (
 		<>
 			<h1 className="font-heading text-center my-12 text-[2rem] font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent">
-				Inertia
+				Inertia: Technical Events
 			</h1>
 			<div className="max-w-[1200px] m-auto my-16 px-4 flex gap-16 flex-wrap justify-center items-center">
 				{EventsData.inertia.map((event, index) => (
