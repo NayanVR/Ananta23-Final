04fe592c42ff1330bf1cf7e326ee356012974a36
NayanVR <nvradadiya281@gmail.com>
2023-03-02 02:15:06 +0530

    dropdown issue fixed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
-import {getAnalytics} from "firebase/analytics"
+import { getAnalytics } from "firebase/analytics"
 
 const app = initializeApp({
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,11 +9,9 @@ const app = initializeApp({
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-
 })
 
 export const auth = getAuth(app);
-export const analytics = getAnalytics(app); 
+export const analytics = getAnalytics(app);
 export const provider = new GoogleAuthProvider();
 export default app
