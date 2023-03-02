e30321868dfcf2adfb6b6ceabb3f5bf14dbf5631
Pratham1777 <prthmpthk777@gmail.com>
2023-03-02 17:50:51 +0530

    spelling

--- client/src/pages/Zingaat.jsx
+++ client/src/pages/Zingaat.jsx
@@ -5,7 +5,7 @@ function Zingaat() {
     return (
         <>
             <h1 className="font-heading text-center my-12 text-[2rem] sm:text-[4rem]  font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent">
-                Zingaat: Cultral Events
+                Zingaat: Cultural Night
             </h1>
             <ComingSoon />
         </>
