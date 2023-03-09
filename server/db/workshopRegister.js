bd73f11b9553d46404369dc0f0e5503d6a0c39b6
Pratham1777 <prthmpthk777@gmail.com>
2023-03-09 10:55:57 +0530

    DR reg closed

--- server/db/workshopRegister.js
+++ server/db/workshopRegister.js
@@ -1,4 +1,18 @@
+const closedWorshops = [
+    "KK_DR"
+]
+
 async function checkForWorkshop(conn, passCode, participantID) {
+
+    if (closedWorshops.includes(passCode)) {
+		return {
+			code: 500,
+            resMessage: {
+                message: "Registration Will Open Soon",
+                type: "info",
+            }
+		};
+	}
 
     console.log(passCode)
 
