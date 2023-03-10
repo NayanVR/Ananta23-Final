2c7313bcab4350fc098107af87c5db4224f99d74
Ashish Kumar Patel <ashishkumarpatel2003@gmail.com>
2023-03-10 12:11:53 +0530

    deep racer done

--- client/src/pages/KalaKriti.jsx
+++ client/src/pages/KalaKriti.jsx
@@ -132,7 +132,7 @@ function KalaKrirti() {
   return (
     <>
       <h1 className="font-heading text-center my-12 text-[2rem] sm:text-[4rem]  font-extrabold bg-gradient-to-b from-primary-light-1 to-primary bg-clip-text text-transparent">
-        KalaKrirti: Workshops
+        Kala Kriti: Workshops
       </h1>
 
       {/* <ComingSoon /> */}
