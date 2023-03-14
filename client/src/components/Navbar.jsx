d09067509433eb3b24feaf5e5c62ecb49f275952
Pratham1777 <prthmpthk777@gmail.com>
2023-03-14 20:58:43 +0530

    all done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -76,12 +76,12 @@ function Navbar() {
                         </DropDown>
                         <CustomLink to="/buypass">Buy a Pass</CustomLink>
                         <CustomLink to="/ananta-coins">Ananta Coins</CustomLink>
-                        
-                        {/* <a href="https://drive.google.com/file/d/1NXGxoK3aH1O6VJS7KPkpC2CAR0nvnpKb/view?usp=share_link" className="p-4 border-t border-primary sm:border-none">
-                Schedule
-        </a> */}
+
+                        <a href="https://drive.google.com/file/d/1SVLIi73PAwvGXNvj-nr1ccVbchwddBQ8/view?usp=share_link" className="p-4 border-t border-primary sm:border-none">
+                            Schedule
+                        </a>
                         <CustomLink to="/our-partners">Our Partners</CustomLink>
-                      
+
                         <DropDown title="More">
                             <Link
                                 className="px-6 py-2 hover:bg-gray-100"
@@ -95,7 +95,7 @@ function Navbar() {
                             >
                                 Our Team
                             </Link>
-                           
+
                         </DropDown>
                     </div>
                     {currentUser ? (
@@ -187,9 +187,9 @@ function Navbar() {
                 <CustomLink to="/buypass">Buy a Pass</CustomLink>
                 <CustomLink to="/ananta-coins">Ananta Coins</CustomLink>
                 {/* <CustomLink to="">Schedule</CustomLink> */}
-                {/* <a href="https://drive.google.com/file/d/1NXGxoK3aH1O6VJS7KPkpC2CAR0nvnpKb/view?usp=share_link" className="p-4 border-t border-primary sm:border-none">
-                Schedule
-        </a> */}
+                <a href="https://drive.google.com/file/d/1SVLIi73PAwvGXNvj-nr1ccVbchwddBQ8/view?usp=share_link" className="p-4 border-t border-primary sm:border-none">
+                    Schedule
+                </a>
                 <CustomLink to="/our-partners">Our Partners</CustomLink>
                 <CustomLink to="/about-us">About Us</CustomLink>
                 <CustomLink to="/contact-us">Our Team</CustomLink>
